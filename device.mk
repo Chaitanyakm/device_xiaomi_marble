42c55d7aa3b12eb2b0989cd7e0e74d1ae40e6a7e
Lunarixus <Nathan@lunarixus.dev>
2023-10-12 17:33:59 +0530

    marble: Nuke Apps during Runtime
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- device.mk
+++ device.mk
@@ -476,6 +476,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.3.vendor \
