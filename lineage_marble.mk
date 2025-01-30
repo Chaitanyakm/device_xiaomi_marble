0b8df646603f5763a374d636b545eeb0162e976c
LuK1337 <priv.luk@gmail.com>
2025-01-30 15:07:41 +0530

    marble: Adjust override prop key names

--- lineage_marble.mk
+++ lineage_marble.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022-2024 The LineageOS Project
+# Copyright (C) 2022-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -16,14 +16,10 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 23049PCD8G
 
-PRODUCT_SYSTEM_NAME := marble_global
-PRODUCT_SYSTEM_DEVICE := marble
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="marble_global-user 14 UKQ1.230804.001 V816.0.9.0.UMRMIXM release-keys" \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
-BUILD_FINGERPRINT := POCO/marble_global/marble:14/UKQ1.230804.001/V816.0.10.0.UMRMIXM:user/release-keys
+    BuildDesc="marble_global-user 14 UKQ1.230804.001 V816.0.14.0.UMRMIXM release-keys" \
+    BuildFingerprint=POCO/marble_global/marble:14/UKQ1.230804.001/V816.0.14.0.UMRMIXM:user/release-keys \
+    DeviceProduct=marble \
+    SystemName=marble_global
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
